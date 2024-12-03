7033fae11804ae82813b14674ff001179209b354
Wohash <iliav@Emonzo>
2024-12-03 16:49:13 +0300

    6

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,36 @@
 import React from "react";
-import Button from "./components/Button"
+
+const showOrders = (props) => {
+    let sum = 0
+    props.orders.forEach(el => sum += Number.parseFloat(el.price));
+  
+    return (
+      <div>
+        {props.orders.map(el => (
+          <Order onDelete={props.onDelete} key={el.id} item={el} />
+        ))}
+  
+        <p onClick={props.AboutForm} className='sum'> 
+          Сумма: {new Intl.NumberFormat().format(sum)} ₽
+        </p>
+  
+        <button onClick={props.togglePaymentForm} className="Buy">
+          Оплатить
+        </button>
+  
+      </div>
+    )
+}
+
+const showNothing = () => {
+    return (
+      <div className='empty'>
+        <h2>
+          Ваша корзина пуста
+        </h2>
+      </div>
+    )
+  }
 
 export default function Header(props) {
     let [cartOpen, setCartOpen] = useState(false);
@@ -8,9 +39,7 @@ export default function Header(props) {
         <header>
             <div>
                 <span className='logo'>All Books Shop</span>
-                <p className='navBar'>
-                  <span className='about'>О нас</span>
-                </p>
+                
                 <p 
                     onClick={() => setCartOpen(!cartOpen)} 
                     className={`shop-button ${cartOpen ? 'active' : ''}`}>
