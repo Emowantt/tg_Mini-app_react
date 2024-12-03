4e282639b134bca6ed8d6f3e876fc3146aa25270
Wohash <iliav@Emonzo>
2024-12-03 16:24:01 +0300

    5

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,19 +1,38 @@
 import React from "react";
 import Button from "./components/Button"
 
-const Header = () => {
-    const tg = window.Telegram.WebApp;
-
-    const onClose = () => {
-        tg.close()
-    }
-
+export default function Header(props) {
+    let [cartOpen, setCartOpen] = useState(false);
+  
     return (
-        <div>
-            <Button onClick={onClose}>Закрыть</Button>
-            <span className="username">
-                {tg.initDataUnsafe?.user?.username}
-            </span>
-        </div>
-    )
+        <header>
+            <div>
+                <span className='logo'>All Books Shop</span>
+                <p className='navBar'>
+                  <span className='about'>О нас</span>
+                </p>
+                <p 
+                    onClick={() => setCartOpen(!cartOpen)} 
+                    className={`shop-button ${cartOpen ? 'active' : ''}`}>
+                    Корзина
+                </p>
+
+                <div className={`shop_Cart ${cartOpen ? 'active' : ''}`}>
+                    {props.orders.length > 0 ?
+                     showOrders(props) : showNothing()}
+                </div>
+
+            </div>
+            <div className='presentation'></div>
+        </header>
+    );
 }
+
+    // return (
+    //     <div>
+    //         <Button onClick={onClose}>Закрыть</Button>
+    //         <span className="username">
+    //             {tg.initDataUnsafe?.user?.username}
+    //         </span>
+    //     </div>
+    // )
