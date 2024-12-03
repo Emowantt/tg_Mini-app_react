c4d24adf450b0387222ee281e4bb21fd8ed13baa
Wohash <iliav@Emonzo>
2024-12-03 18:28:09 +0300

    7

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { useEffect } from 'react';
 import Header from './components/Header/Header';
 import './App.css';
+import { useTelegram } from './hooks/useTelegram';
 
 const tg = window.Telegram.WebApp;
 
 
 function App() {
+  const {onToggleButton, tg} = useTelegram();
 
   useEffect(() => {
     tg.ready();
@@ -13,7 +15,7 @@ function App() {
 
   return (
     <div className="App">
-      <Header/>
+      <button onClick={onToggleButton}>toggle</button>
     </div>
   );
 }
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,17 +1,14 @@
 import React from "react";
 import './header.css';
 import Button from "../Button/Button";
+import { useTelegram } from "../../hooks/useTelegram";
 
 function Header () {
-    
-    const tg = window.Telegram.WebApp;
 
-    const onClose = () => {
-        tg.close()
-    }
+    const {user, onClose} = useTelegram();
 
     return (
-        <div>
+        <div className="header">
             <Button onClick={onClose}>Закрыть</Button>
             <span className="username">
                 {tg.initDataUnsafe?.user?.username}
